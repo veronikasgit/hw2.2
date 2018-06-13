5ce76d1ac744816b2b8847939ab0be2f3006a6ad
Veronika <w_e_r_o_n_i_k_a@mail.ru>
2018-06-14 02:17:11 +0300

    Initial commit.

--- list.php
+++ list.php
@@ -1,16 +1,16 @@
 <?php
 
-$json = file_get_contents(__DIR__ . '/content1.json');
+$json = file_get_contents(__DIR__ . '/content.json');
 $data = json_decode($json, true);
 echo('<pre>');
 print_r($data);
 
 echo '<ol>';
-foreach ($data as $item) {
-
-  echo '<li><a href="test.php?id='. $item.'">'.$item['question'].'</a></li>'.'<br>';
+foreach ($data as $key => $item) {
+  foreach ($item as $avalue) {
+    echo '<li><a href="test.php?id='. $key.'">'.$value['question'].'</a></li>'.'<br>';
+  };
 };
-
 echo '</ol>';
 ?>
 
--- test.php
+++ test.php
@@ -1,21 +1,34 @@
 <?php
 
-$json = file_get_contents(__DIR__ . '/content1.json');
+$json = file_get_contents(__DIR__ . '/content2.json');
 $data = json_decode($json, true);
-
-foreach ($data as $key => $item) {
-
-$variantsOfAnswers = $item['variantsOfAnswers'];
-		
+ 
+$x = $_GET['id'];
+ 
+$variantsOfAnswers = $data[$x][$x]['variantsOfAnswers'];
+ 
 $a = $variantsOfAnswers['a'];
 $b = $variantsOfAnswers['b'];
 $c = $variantsOfAnswers['c'];
 $d = $variantsOfAnswers['d'];
  
-$question = $item['question'];
-$rightAnswer = $item['rightAnswer'];
+$question = $data[$x][$x]['question'];
+$rightAnswer = $data[$x][$x]['rightAnswer'];
 
-}; 
+if (isset($_GET['q1'])) {
+	echo $_GET['q1']. '<br>';
+	$_SESSION['variantsOfAnswers'] = $_GET['q1'];
+	echo $_SESSION['variantsOfAnswers']. '<br>'; 
+		 if ($_SESSION['variantsOfAnswers'] === $rightAnswer) {
+		 	echo "Верно"; 
+			} else {
+			echo "НеВерно"; 
+
+			};
+
+
+};
+
  
 ?>
  
@@ -23,22 +36,11 @@ $rightAnswer = $item['rightAnswer'];
 <form action="" method="GET" enctype="multipart/form-data">
 
 	<fieldset>
-        <legend>Сколько граммов в одном килограмме?</legend>
-	    <label><?php echo $question?></label>
+        <legend><?php echo $question; ?></legend>
 	    <label><input type="radio" name="q1" value="$a"><?php echo $a; ?></label>
 	    <label><input type="radio" name="q1" value="$b"><?php echo $b; ?></label>
 	    <label><input type="radio" name="q1" value="$c"><?php echo $c; ?></label>
 	    <label><input type="radio" name="q1" value="$d"><?php echo $d; ?></label>
     </fieldset>
-    <button type="submit">Проверить</button>
+    <button type="submit" name="button">Проверить</button>
 </form>
-
-<?php
-var_dump($_GET);
-if(!isset($_GET['question']) || !file_exists('./test.php/' . $_GET['question']))
-  exit('Invalid test question');
-$item = $_GET['question'];
-var_dump($_GET);
-showTest($data['test']);
-
-?>
